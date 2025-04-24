1cdc58052c91960e192e67118a4d3378c6bf4b62
Bhadri2015-SB <bhadri2919@gmail.com>
2025-04-25 01:57:20 +0530

    Refactor database.py to improve MySQLdb installation and organization

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import dotenv, os
-
+import pymysql
+pymysql.install_as_MySQLdb() 
 # DATABASE_URL = "mysql+pymysql://root:yourpassword@localhost/yourdatabase"
 dotenv.load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
