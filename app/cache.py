80772f3cea77642a9c694d09b24ca2abe17ffeac
Bhadri2015-SB <bhadri2919@gmail.com>
2025-04-24 10:32:07 +0530

    Refactor Redis connection to use environment variables for host and port

--- app/cache.py
+++ app/cache.py
@@ -1,7 +1,13 @@
 import redis
 import json
+import dotenv
+import os
 
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+dotenv.load_dotenv()
+host = os.getenv("REDIS_HOST")
+port = os.getenv("REDIS_PORT")
+
+r = redis.Redis(host=host, port=port, db=0, decode_responses=True)
 
 def get_user_cache(email: str):
     user = r.get(f"user:{email}")
